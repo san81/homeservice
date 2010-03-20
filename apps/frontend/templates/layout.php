07bd410321f1a558c999b4aee9e310c3a4ef6764
santhosh.gandhe <santhosh.gandhe@5860284c-1a64-11df-b1cb-2be540f5faec>
2010-03-20 09:15:57 +0000

    layout addjusted

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,6 +14,43 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+	<div id="page_header">
+		<div id="page_title">
+		<h1>Ultimate Service Solutions</h1>
+		</div>
+		<div id="header_search">
+			<form method="post" action="http://www.uss.spconsultancy.com">
+			<div>
+			<h3><span>Find:</span></h3>
+			<input type="text" />
+			<input type="image" src="img/search_button.gif" class="submit" />
+			</div>
+			</form>
+		</div>
+	</div>
+	<div id="page_spacing">
+		<div id="page_header1">
+			<div id="links">
+				<span><a href="<?php echo url_for('items/index') ?>">Items</a></span>
+				<span><a href="<?php echo url_for('areas/index') ?>">Areas</a></span>
+				<span><a href="<?php echo url_for('zones/index') ?>">Zones</a></span>
+				<span><a href="<?php echo url_for('customer/index') ?>">Customers</a></span>
+				<span><a href="<?php echo url_for('employee/index') ?>">Employees</a></span>
+				<span><a href="<?php echo url_for('complaints/index') ?>">Complaints</a></span>
+			</div>
+		</div>
+	</div>
+   <div id="page_wrapper">
+	<!-- BEGIN :: LEFT SIDEBAR -->
+	<div id="page_leftcol"></div>
+	<!-- BEGIN :: MAIN COL -->
+	<div id="page_maincol">
+		<div id="bestsellers">
+			<?php echo $sf_content ?>
+		</div>
+	</div>
+	<!-- BEGIN :: RIGHT SIDEBAR -->
+	<div id="page_rightcol"></div>
+   </div>
   </body>
 </html>
